2fc94363cfbfbc224d7458f6be06af50e06f254f
pslaulhe <pslaulhe@gmail.com>
2024-05-22 09:07:47 +0200

    Migrate

--- Typescript/src/Field/ImageThisIsAClientInADifferentRepository.ts
+++ Typescript/src/Field/ImageThisIsAClientInADifferentRepository.ts
@@ -3,7 +3,7 @@ import { ShoppingCart } from "./ShoppingCart";
 export class ImagineThisIsAClientInADifferentRepository {
   FormattedPrice = () => {
     var cart = new ShoppingCart();
-    cart.add(10);
-    return `Total price is ${cart.calculateTotalPrice()} euro`;
+    cart.addItems(10);
+    return `Total price is ${cart.calculateNewTotalPrice()} euro`;
   };
 }
